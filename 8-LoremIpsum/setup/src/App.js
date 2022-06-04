6b93cb751fc9fa843f1b4b292ff8a8ec6e1cee60
Dmytrii Furs <furs.dmitry@gmail.com>
2022-06-04 00:52:39 -0400

    8-LoremIpsum

---
+++ 8-LoremIpsum/setup/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+
+  const checkCount = (amount) => {
+    if(amount > data.length)
+      amount = data.length;
+    if(amount <= 0)
+      amount = 1;
+    
+    return amount;
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      let amount = parseInt(count);
+
+      setText(data.slice(0, checkCount(amount)));
+  };
+
+  return (
+  <section className='section-center'>
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='amount'>
+            Paragraphs: 
+          </label>
+          <input type='number' name='amount' id='amount' 
+          value={count}
+          onChange={(e)=> {
+              setCount(e.target.value);
+          }}/>
+          <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article className='lorem-text'>
+          {text.map((item, index)=> {
+            return <p key={index}>{item}</p>
+          })}
+      </article>
+  </section>
+  )
+}
+
+export default App;
