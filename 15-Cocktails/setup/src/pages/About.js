1ef0687bb09368a36edf20986ec0549c36452f7b
Dmytrii Furs <furs.dmitry@gmail.com>
2022-06-18 19:30:18 -0400

    15-Cocktails

---
+++ 15-Cocktails/setup/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+          Some useful information...
+      </p>
+    </section>
+  )
+}
+
+export default About
